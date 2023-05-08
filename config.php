e45dbfdc06c40d989e7da27ed217d4222bd4daca
weikang <weikang.ng@evdg.cc>
2023-05-08 08:43:45 +0800

    map add database

---
+++ config.php
@@ -0,0 +1,8 @@
+<?php
+
+$dbservername = "localhost";
+$dbusername = "root";
+$dbpassword ="";
+$dbname = "testmap";
+
+$conn = mysqli_connect($dbservername,$dbusername,$dbpassword,$dbname);
--- find-dealer.php
+++ find-dealer.php
@@ -1,3 +1,11 @@
+<?php
+// Include the database configuration file
+require_once 'config.php';
+
+// Fetch the marker info from the database
+$result = $conn->query("SELECT * FROM map"); // Fetch the info-window data from the $result2 =
+$conn->query("SELECT * FROM map"); ?>
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
@@ -83,9 +91,7 @@
               class="nav-item nav-link nav-pd fw-bolder">
               Tech & Knowledge
             </a>
-            <a
-              href="contact-us.php"
-              class="nav-item nav-link nav-pd fw-bolder">
+            <a href="contact-us.php" class="nav-item nav-link nav-pd fw-bolder">
               Contact Us
             </a>
           </div>
@@ -94,7 +100,7 @@
     </div>
     <!-- Topbar End -->
 
-    <div class="find-dealer-container">
+    <div class="find-dealer-container mb-6">
       <section class="find-dealer-search-section">
         <div class="find-dealer-search">
           <div class="find-dealer-search-container">
@@ -238,11 +244,211 @@
 
     <!-- Footer Start -->
     <?php include 'footer.php'?>
-   <!-- Footer End -->
+    <!-- Footer End -->
   </body>
 
-  <script src="static/js/map.js"></script>
   <script
     async
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyB94E1buyq9HpajiO6d8EA8REcskcoAmxw&callback=initMap"></script>
+  <script>
+        function initMap() {
+          const myLatLng = { lat: 2.4641896090638924, lng: 102.91983144134475 };
+          const map = new google.maps.Map(document.getElementById("map"), {
+          zoom: 12,
+          center: myLatLng,
+      });
+
+      // const points = [
+      //   [
+      //     "1",
+      //     "A",
+      //     2.5087898859664874,
+      //     102.81350169244817,
+      //     `<p class="text-black">This is A</p>`,
+      //   ],
+      //   [
+      //     "2",
+      //     "B",
+      //     2.49374240185527,
+      //     102.85897342292282,
+      //     `<p class="text-black">This is B</p>`,
+      //   ],
+      //   [
+      //     "3",
+      //     "C",
+      //     2.5011472088163056,
+      //     102.85658834825935,
+      //     `<p class="text-black">This is C</p>`,
+      //   ],
+      //   [
+      //     "4",
+      //     "D",
+      //     2.4962957222302466,
+      //     102.84687128163006,
+      //     `<p class="text-black">This is D</p>`,
+      //   ],
+      //   [
+      //     "5",
+      //     "E",
+      //     2.497437795877134,
+      //     102.82664195090041,
+      //     `<p class="text-black">This is E</p>`,
+      //   ],
+      //   [
+      //     "6",
+      //     "F",
+      //     2.5240773782823474,
+      //     102.81155653701249,
+      //     `<p class="text-black">This is F</p>`,
+      //   ],
+      // ];
+      const points = [
+        <?php if($result->num_rows > 0){
+          while($row = $result->fetch_assoc()){
+            echo '["'.$row['id'].'",
+            "'.$row['name'].'",
+            '.$row['latitude'].',
+            '.$row['longitude'].',
+            `<p class="text-black">'.$row['info'].'</p>`], ';
+            }
+        }?>
+      ];
+
+      let normalIcon = {
+        url: "http://localhost:8080/chariox/static/images/other/map-marker.svg",
+        scaledSize: new google.maps.Size(50, 50), // scaled size
+      };
+      let selectedIcon = {
+        url: "http://localhost:8080/chariox/static/images/other/map-marker-selected.svg",
+        scaledSize: new google.maps.Size(50, 50), // scaled size
+      };
+
+      for (var i = 0; i < points.length; i++) {
+        const marker = new google.maps.Marker({
+          position: {
+            lat: points[i][2],
+            lng: points[i][3],
+          },
+          map: map,
+          icon: normalIcon,
+          label: {
+            text: points[i][1],
+            color: "#000000",
+            className: "marker-label",
+          },
+          id: points[i][0], // Set the marker's ID
+        });
+        const detailWindow = new google.maps.InfoWindow({
+          content: points[i][4],
+        });
+
+        marker.addListener("mouseover", () => {
+          detailWindow.open(map, marker);
+
+          if (mouseoverInfoWindow) {
+            mouseoverInfoWindow.close();
+          }
+          detailWindow.open(map, marker);
+          mouseoverInfoWindow = detailWindow;
+        });
+
+        const resultcard = document.querySelectorAll(".markericon");
+        const markerIds = ["1", "2", "3", "4", "5", "6"];
+        const resultCardBox = document.querySelectorAll(".find-dealer-result-card");
+
+        var activeInfoWindow;
+        var mouseoverInfoWindow;
+        var selectedMarker;
+        var prevSelectedElement;
+
+        function activeInfoWindows() {
+          if (activeInfoWindow) {
+            activeInfoWindow.close();
+          }
+
+          detailWindow.open(map, marker);
+          activeInfoWindow = detailWindow;
+        }
+
+        function selectedMarkers() {
+          if (selectedMarker) {
+            selectedMarker.setIcon(normalIcon);
+          }
+          marker.setIcon(selectedIcon);
+          selectedMarker = marker;
+        }
+
+        function toggleMarkerIconSelected(resultcard, index) {
+          resultcard.forEach((el) => {
+            el.classList.remove("markericon-selected");
+          });
+          resultcard[index].classList.toggle("markericon-selected");
+        }
+
+        for (let i = 0; i < resultCardBox.length; i++) {
+          for (let i = 0; i < resultcard.length; i++) {
+            marker.addListener("click", function () {
+              map.setZoom(16);
+              map.setCenter(marker.getPosition());
+
+              if (markerIds[i] === marker.id) {
+                console.log(marker.id);
+                const element = document.getElementById(marker.id);
+                if (element) {
+                  element.scrollIntoView();
+
+                  if (prevSelectedElement) {
+                    prevSelectedElement.classList.remove(
+                      "find-dealer-result-card-selected"
+                    );
+                  }
+                  element.classList.toggle("find-dealer-result-card-selected");
+                  prevSelectedElement = element;
+                } else {
+                  console.error(`Element with id '${marker.id}' not found`);
+                }
+
+                if (selectedMarker && selectedMarker !== marker) {
+                  const selectedElement = document.getElementById(
+                    selectedMarker.id
+                  );
+
+                  if (selectedElement) {
+                    selectedElement.classList.remove(
+                      "find-dealer-result-card-selected"
+                    );
+                  }
+                }
+
+                toggleMarkerIconSelected(resultcard, i);
+                activeInfoWindows();
+                selectedMarkers();
+              }
+            });
+
+            resultCardBox[i].addEventListener("click", function () {
+              if (markerIds[i] === marker.id) {
+                map.setZoom(16);
+                map.setCenter(marker.getPosition());
+                const element = document.getElementById(marker.id);
+
+                if (prevSelectedElement) {
+                  prevSelectedElement.classList.remove(
+                    "find-dealer-result-card-selected"
+                  );
+                }
+
+                element.classList.toggle("find-dealer-result-card-selected");
+                prevSelectedElement = element;
+
+                toggleMarkerIconSelected(resultcard, i);
+                activeInfoWindows();
+                selectedMarkers();
+              }
+            });
+          }
+        }
+      }
+    }
+  </script>
 </html>
